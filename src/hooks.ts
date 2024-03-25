6aed50ddb1ee56d82779c1a17adac701f74d2a93
Yume <hei.hajatiana@gmail.com>
2024-03-25 15:14:26 +0300

    fix: hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,7 +1,7 @@
 import {useQuery}  from "@tanstack/react-query"
 import axios from "axios";
 
-type Data = Record<string, string>
+type Data = Record<string, string>;
 
 const BASE_URL = "https://jsonplaceholder.typicode.com";
 
