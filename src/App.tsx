f3955f2d2abad470500fc2b64d7085878bf34861
Yume <hei.hajatiana@gmail.com>
2024-03-25 14:43:59 +0300

    feat: data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,75 @@
-import {useForm, SubmitHandler} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod"
-import z from "zod";
-import './App.css'
+import axios from "axios";
+import {useEffect, useState} from "react";
 
-const userSchema = z.object({
-  username: z.string().min(1).max(20),
-  age: z.number().gt(1).lt(500),
-  email: z.string().email(),
-});
+const BASE_URL = "https://jsonplaceholder.typicode.com";
 
-type UserSchema = z.infer<typeof userSchema>;
+type Data = Record<string, string>
 
-const App = () => {
-  const {register, handleSubmit, formState: { errors }} = useForm({
-    defaultValues: {
-      username: "",
-      age: 17,
-      email: "example@gmail.com"
-    },
-    resolver: zodResolver(userSchema),
-  });
+export default function App() {
+  const [users, setUsers] = useState<Data[]>([]);
+  const [posts, setPosts] = useState<Data[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const onSubmit: SubmitHandler<UserSchema> = (data) => {
-    console.log('data', data);
-  }
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        // fetch
+        // axios
+        // super-agent
+        const response = await axios.get(BASE_URL + '/users')
+        setUsers(response.data);
+      } catch (e) {
+        console.error('get user: ', e);
+      }
+
+    }
+
+    const getPosts = async () => {
+      try {
+        // fetch
+        // axios
+        // super-agent
+        const response = await axios.get(BASE_URL + '/posts')
+        setPosts(response.data);
+      } catch (e) {
+        console.error('get user: ', e);
+      }
+
+
+    }
+
+    (async () => {
+      setIsLoading(true);
+      await getUsers();
+      await getPosts();
+      setIsLoading(false);
+    })()
+
+  }, []);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <input {...register("username")} />
-        <div>{errors.username?.message}</div>
-      </div>
+    <div style={{ display: "flex", gap: 2 }}>
+      {isLoading && <h1>
+        loading...
+      </h1>}
 
       <div>
-        <input type="number" {...register("age")} />
-        <div>{errors.age?.message}</div>
-      </div>
+        <h1>users</h1>
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>{user.username}</li>
+          ))}
+        </ul>
+       </div>
 
       <div>
-        <input type="email" {...register("email")} />
-        <div>{errors.email?.message}</div>
-      </div>
-
-      <button type="submit">submit</button>
-    </form>
-  );
-} 
-
-export default App
-
+        <h1>posts</h1>
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+       </div>
+    </div>
+  )
+}
